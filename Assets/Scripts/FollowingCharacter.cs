77bf957bf9d753a71f55185f269ea849c71bbbd2
Sergio <Sergio.escuder5@gmail.com>
2019-11-05 19:24:46 +0100

    Pushing Advanced Project

---
+++ Assets/Scripts/FollowingCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCharacter : MonoBehaviour
+{
+    ////////////////////////////
+    /// /////////////////////////////------------------------------VARIABLES
+    ////////////////////////////
+
+    [SerializeField] Transform player;
+    [SerializeField] Rigidbody targetRB;
+    [SerializeField] float diferenceHeight;
+    [SerializeField] float cameraSpeed;
+
+    ////////////////////////////
+    /// /////////////////////////////------------------------------METHODS
+    ////////////////////////////
+
+
+    /// /////////////////---- START
+    void Start()
+    {
+        
+    }
+
+    /// /////////////////---- UPDATE
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, player.position.y + diferenceHeight, transform.position.z);
+        targetRB.MovePosition(newPosition);
+        //diferenceHeight = transform.position.y - player.position.y;
+        //Debug.Log(diferenceHeight);
+        //if (diferenceHeight < 0 && diferenceHeight < -maxHeight)
+        //{
+        //    transform.Translate(transform.position.x, transform.position.y - cameraSpeed, transform.position.z);
+        //}
+        //else if (diferenceHeight > 0 && diferenceHeight > maxHeight)
+        //{
+        //    transform.Translate(transform.position.x, (transform.position.y - cameraSpeed), transform.position.z);
+        //}
+    }
+}
