e989930eab97b572d177f1a5c92257ad3d5b9307
Sergio <sergioescudergallego@enti.cat>
2019-11-07 13:34:19 +0100

    Improving Level 1

---
+++ Assets/Scripts/SwitchLevel1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLevel1 : MonoBehaviour
+{
+    #region VARIABLES
+    [SerializeField] GameObject light;
+    #endregion
+
+    #region ON TRIGGER ENTER/EXIT
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            light.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            light.SetActive(false);
+        }
+    }
+    #endregion
+}
