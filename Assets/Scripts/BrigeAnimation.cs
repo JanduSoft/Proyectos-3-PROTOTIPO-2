be0003889332523e07e395a6e4661490cd683dc9
Sergio <Sergio.escuder5@gmail.com>
2019-11-10 20:06:29 +0100

    Level 3 Animations & platforms Done

--- Assets/Scripts/BrigeAnimation.cs
+++ Assets/Scripts/BrigeAnimation.cs
@@ -5,13 +5,43 @@ using UnityEngine;
 public class BrigeAnimation : MonoBehaviour
 {
     [SerializeField] Animator bridgeAnimtor;
+    private bool isInside= false;
+    [SerializeField] GameObject textButton;
 
+    #region UPDATE
+    private void Update()
+    {
+        if (isInside)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                bridgeAnimtor.SetBool("Active", true);
+            }
+        }
+    }
+    #endregion
 
+    #region TRIGGER DETECTION
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
+            isInside = true;
+            textButton.SetActive(true);
+            //bridgeAnimtor.SetBool("Active" , true);
+        }
         if (other.CompareTag("Hook"))
         {
             bridgeAnimtor.SetBool("Active" , true);
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInside = false;
+            textButton.SetActive(false);
+        }
+    }
+    #endregion
 }
