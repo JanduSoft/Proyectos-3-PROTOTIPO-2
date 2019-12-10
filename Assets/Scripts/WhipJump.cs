37482d017afbf93403915d309c73e34567dfd73e
Aleix <aleix.azuela@enti.cat>
2019-12-10 13:46:56 +0100

    Whip Jump started not Done yet

---
+++ Assets/Scripts/WhipJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhipJump : MonoBehaviour
+{
+    [SerializeField] Transform destinationForJump;
+    [SerializeField] GameObject player;
+    Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPlayerTransform(Transform transform)
+    {
+        playerTransform = transform;
+        player.SendMessage("setDestinationTransform", destinationForJump);
+        player.SendMessage("setWhipableJumpObjectTransform", this.transform);
+    }
+}
