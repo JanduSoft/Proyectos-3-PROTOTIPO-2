f9ab568cf41c3ec0352c16cc6c253f7da21cb8ad
Aleix <aleix.azuela@enti.cat>
2019-12-17 11:58:32 +0100

    NavMesh Imported, arrows working needs polishing

---
+++ Assets/Scripts/Enemies/StaticEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject ArrowPrefab;
+    [SerializeField] float viewingAngle;
+    [SerializeField] float viewingDistance;
+    [SerializeField] float shotTiming;
+    [SerializeField] float angleBetweenEnemyandPlayer = 0;
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angleBetweenEnemyandPlayer = Vector3.Angle(transform.forward, new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z) - transform.position);
+        time += Time.deltaTime;
+        if (angleBetweenEnemyandPlayer < viewingAngle)
+        {
+            if(time > shotTiming)
+            {
+                Instantiate(ArrowPrefab, transform.position, Quaternion.LookRotation(transform.forward), transform);
+                time = 0;
+            }
+        }
+        
+    }
+}
