0a6eb5d4bf92ac8769a274467453749d72d9589d
Aleix <aleix.azuela@enti.cat>
2019-12-17 13:33:18 +0100

    Slow Rotation Started not done yet

--- Assets/Scripts/Enemies/StaticEnemy.cs
+++ Assets/Scripts/Enemies/StaticEnemy.cs
@@ -24,6 +24,8 @@ public class StaticEnemy : MonoBehaviour
         time += Time.deltaTime;
         if (angleBetweenEnemyandPlayer < viewingAngle)
         {
+            Quaternion finalRot = Quaternion.LookRotation(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z) - transform.position);
+            Vector3 newRotV3 = Vector3.Lerp(transform.rotation, new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z) - transform.position, 0.5f);
             if(time > shotTiming)
             {
                 Instantiate(ArrowPrefab, transform.position, Quaternion.LookRotation(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z) - transform.position), transform);
