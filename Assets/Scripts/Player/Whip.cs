395351efcf7e1116632728c80864bb7050350343
Aleix <aleix.azuela@enti.cat>
2019-12-21 12:20:51 +0100

    ErrorFixed

--- Assets/Scripts/Player/Whip.cs
+++ Assets/Scripts/Player/Whip.cs
@@ -62,7 +62,7 @@ public class Whip : MonoBehaviour
         {
             whip.SetPosition(1, whipableObjectTransform.position);
 
-            float x = Mathf.Lerp(0, distToDestination, 0.035f);
+            float x = Mathf.Lerp(0, distToDestination, 0.025f);
 
             Vector3 pA = playerTransform.position;
 
