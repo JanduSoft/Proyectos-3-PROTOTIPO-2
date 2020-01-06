cefb8d5305a49a281cde00cebe00a31482484dea
Sergio <Sergio.escuder5@gmail.com>
2020-01-06 20:54:18 +0100

    Part 1 & Part 2 80%

---
+++ Assets/Scripts/EnablePartsOfMap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePartsOfMap : MonoBehaviour
+{
+    enum typeOfGO
+    {
+        NONE,
+        TO_ENABLE,
+        TO_DISABLE
+    }
+    [SerializeField] typeOfGO typeGO;
+    [SerializeField] GameObject go;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (typeGO)
+            {
+                case typeOfGO.NONE:
+                    break;
+                case typeOfGO.TO_ENABLE:
+                    {
+                        go.SetActive(true);
+                        break;
+                    }
+                case typeOfGO.TO_DISABLE:
+                    {
+                        go.SetActive(false);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
