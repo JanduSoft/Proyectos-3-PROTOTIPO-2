c36db27a02c7962e97ebf8f15902004c32bd922d
Aleix <aleix.azuela@enti.cat>
2020-01-08 03:07:30 +0100

    tUTORIAL pART 2 MODIFICATIONS dONE AND WORKING

---
+++ Assets/Scripts/TempleTut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleTut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject templeDoor;
+    [SerializeField] Camera cam;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            templeDoor.SetActive(false);
+            cam.clearFlags = CameraClearFlags.Color;
+        }
+    }
+}
