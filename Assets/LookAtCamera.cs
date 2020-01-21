de5fac82df66d125dd40ce92f3549bd2cc7be1fc
Sergio <Sergio.escuder5@gmail.com>
2020-01-21 11:09:09 +0100

    NewWhipJump

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] Transform camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera.position);
+    }
+}
