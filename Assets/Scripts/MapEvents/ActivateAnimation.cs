ff75b10b814a60e0041a0976962f039ded4f8d86
Sergio <Sergio.escuder5@gmail.com>
2020-01-24 10:13:09 +0100

    Whip & Platforma Animation Fixed

--- Assets/Scripts/MapEvents/ActivateAnimation.cs
+++ Assets/Scripts/MapEvents/ActivateAnimation.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class ActivateAnimation : MonoBehaviour
 {
+    public enum typeAnimator
+    {
+        NONE,
+        DOOR,
+        PLATFORM
+    }
     [SerializeField] Animator myAnimator;
+    [SerializeField] typeAnimator type;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Skull"))
+        if (other.CompareTag("Skull") && type == typeAnimator.DOOR )
         {
             myAnimator.SetBool("Active", true);
             Invoke("DeactivateAnimation", 2f);
+        }
+        else if (other.CompareTag("Player") && type == typeAnimator.PLATFORM)
+        {
+            myAnimator.SetBool("Active", true);
+            Invoke("DeactivateAnimation", 2.5f);
         }
     }
 
