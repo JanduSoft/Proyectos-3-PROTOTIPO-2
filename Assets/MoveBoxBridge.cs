79b01145fb54ebc721bc9d070821d85de8af3443
Sergio <Sergio.escuder5@gmail.com>
2020-02-06 20:43:43 +0100

    Movable Box & Part1 Greyboxing

---
+++ Assets/MoveBoxBridge.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveBoxBridge : MonoBehaviour
+{
+    bool canActivate = false;
+
+    [SerializeField] Transform box;
+    [SerializeField] Transform position1;
+    [SerializeField] Transform position2;
+    [SerializeField] float speed;
+    int position = 1;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && canActivate)
+        {
+            if (position == 1)
+            {
+                position = 2;
+                box.DOMove(position2.position, speed);
+            }
+            else
+            {
+                position = 1;
+                box.DOMove(position1.position, speed);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canActivate = true;
+        }
+    }
+}
