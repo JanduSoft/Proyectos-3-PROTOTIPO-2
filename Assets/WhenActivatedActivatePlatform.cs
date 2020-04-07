3e0407926473ce3c326d3c1ff5940a9f166d8d12
Cristina Gil <cristinagbotey@gmail.com>
2020-04-07 15:04:05 +0200

    first mini puzzle done

---
+++ Assets/WhenActivatedActivatePlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhenActivatedActivatePlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject targetActivate;
+    bool gobletActivated = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<AddSkull>().isActivated && !gobletActivated)
+        {
+            gobletActivated = true;
+            targetActivate.SendMessage("ActivateObject", false, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
