4355c4c2a57accd00bf58aac74313b7447b54f4e
Sergio <Sergio.escuder5@gmail.com>
2020-04-24 20:58:22 +0200

    Initial Cinematic Tut2 2

---
+++ Assets/InitialCinematic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialCinematic : MonoBehaviour
+{
+    [SerializeField] PlayerMovement player;
+    [SerializeField] float timeCinematic = 15;
+    void Start()
+    {
+        player.StopMovement(true);
+        Invoke("DestroyCinematic", timeCinematic);
+    }
+
+    void DestroyCinematic()
+    {
+        player.StopMovement(false);
+        Destroy(this.gameObject);
+    }
+}
