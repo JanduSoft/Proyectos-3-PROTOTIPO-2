815dd205b72b91ce2f17623467181d10cf4fd262
Aleix <aleix.azuela@enti.cat>
2020-04-27 20:02:53 +0200

    Rock now destroys the arrows

--- Assets/Scripts/CrashWithObstacle.cs
+++ Assets/Scripts/CrashWithObstacle.cs
@@ -70,5 +70,58 @@ public class CrashWithObstacle : MonoBehaviour
 
         }
     }
+     private void OnTriggerStay(Collider other)
+    {
+        for (int i = 0; i < obstacle.Length; i++)
+        {
+            switch (obstacle[i])
+            {
+                case ObstacleType.NONE:
+                    {
+                        break;
+                    }
+                case ObstacleType.OBSTACLE:
+                    {
+                        if (other.CompareTag("Block"))
+                        {
+                            Destroy(this.gameObject);
+                            Destroy(Instantiate(particlesDestroy, this.transform.position, Quaternion.identity), 1f);
+                        }
+                        break;
+                    }
+                case ObstacleType.WALL:
+                    {
+                        if (other.CompareTag("Wall"))
+                        {
+                            Destroy(this.gameObject);
+                            Destroy(Instantiate(particlesDestroy, this.transform.position, Quaternion.identity), 1f);
+                        }
+                        break;
+                    }
+                case ObstacleType.FLOOR:
+                    {
+                        if (other.CompareTag("Floor"))
+                        {
+                            Destroy(this.gameObject);
+                            Destroy(Instantiate(particlesDestroy, this.transform.position, Quaternion.identity), 1f);
+                        }
+                        break;
+                    }
+                case ObstacleType.ANY:
+                    {
+                        if (other.CompareTag("Stone") || other.CompareTag("Wall") || other.CompareTag("Floor"))
+                        {
+                            Destroy(this.gameObject);
+                            Destroy(Instantiate(particlesDestroy, this.transform.position, Quaternion.identity), 1f);
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+
+        }
+    }
 
 }
