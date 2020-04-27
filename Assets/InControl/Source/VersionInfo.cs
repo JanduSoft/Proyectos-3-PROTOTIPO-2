dd3233d34a8a3544033b8791970847e9374cfdd4
Sergio <Sergio.escuder5@gmail.com>
2020-04-27 04:57:57 +0200

    Main Menu & Save Game & Pause Menu, To do Resolution and Graphics

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-				Build = 5522
+				Build = 5569
 			};
 		}
 
--- Assets/Scripts/Game Manager/SendToLevel1.cs
+++ Assets/Scripts/Game Manager/SendToLevel1.cs
@@ -8,7 +8,7 @@ public class SendToLevel1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("TestTutorialPart1");
     }
 
     // Update is called once per frame
