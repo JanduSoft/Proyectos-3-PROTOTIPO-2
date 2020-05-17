ce8429ff0381b8006e45311139c1813f3a622659
Sergio <Sergio.escuder5@gmail.com>
2020-05-17 13:37:38 +0200

    tIMElOGdONE

---
+++ Assets/SavingTime.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SavingTime : MonoBehaviour
+{
+    public enum Moment
+    {
+        NONE,
+        START,
+        END
+    };
+
+    [SerializeField] Moment currentMoment;
+
+    void CreateText()
+    {
+        //Path of the file
+        string path = Application.dataPath + "/RESULTADOS/TimeLog.txt";
+
+        //Delete File if it exist
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+        File.WriteAllText(path, "Time Start:  "+System.DateTime.Now + "\n\n");
+        
+    }
+
+    void AddFinalTime()
+    {
+        //Path of the file
+        string path = Application.dataPath + "/RESULTADOS/TimeLog.txt";
+
+        //Add time to the file
+        File.AppendAllText(path, "Time Finished:  " + System.DateTime.Now + "\n\n");
+    }
+
+    void Start()
+    {
+        if (currentMoment == Moment.START)
+        {
+            CreateText();
+        }
+        else if (currentMoment == Moment.END)
+        {
+            AddFinalTime();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Thanks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Thanks : MonoBehaviour
+{
+    [SerializeField] float time = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GoToMainMenu", time);
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
