5d0f5773bfbb8295bb0c7138e7a0bc11ba3a3ceb
Cristina Gil <cristinagbotey@gmail.com>
2020-05-26 22:11:45 +0200

    pushing rock sensitivity fixed

--- Assets/Scripts/rocksection1.cs
+++ Assets/Scripts/rocksection1.cs
@@ -5,48 +5,59 @@ using UnityEngine;
 public class rocksection1 : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] GameObject part1;
-    [SerializeField] GameObject part2;
+    //[SerializeField] GameObject part1;
+    //[SerializeField] GameObject part2;
     [SerializeField] GameObject rock;
-    [SerializeField] Transform point1;
-    [SerializeField] Transform point2;
-    Vector3 centerPoint;
+    //[SerializeField] Transform point1;
+    //[SerializeField] Transform point2;
+    //Vector3 centerPoint;
+
+    [SerializeField] Transform parentPart;
     
     void Start()
     {
-        Vector3 uv = (point2.position - point1.position) / 2;
-        centerPoint = point1.position + uv;
+        //Vector3 uv = (point2.position - point1.position) / 2;
+        //centerPoint = point1.position + uv;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Stone") && !other.isTrigger)
+        {
+            Debug.Log("Entered");
+            rock.transform.SetParent(parentPart);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rock.transform.position.z <= centerPoint.z)
-        {
-            //rock child of part 1
-            rock.transform.SetParent(part1.transform);
+        //if (rock.transform.position.z <= centerPoint.z)
+        //{
+        //    //rock child of part 1
+        //    rock.transform.SetParent(part1.transform);
             
-        }
-        else if(rock.transform.position.z>centerPoint.z && part2.activeSelf)
-        {
-            //rock child of part 2
-            rock.transform.SetParent(part2.transform);
+        //}
+        //else if(rock.transform.position.z>centerPoint.z && part2.activeSelf)
+        //{
+        //    //rock child of part 2
+        //    rock.transform.SetParent(part2.transform);
 
-        }
-        else if (rock.transform.position.z > centerPoint.z && part1.activeSelf)
-        {
-            //rock child of part 1
-            rock.transform.SetParent(part1.transform);
-        }
+        //}
+        //else if (rock.transform.position.z > centerPoint.z && part1.activeSelf)
+        //{
+        //    //rock child of part 1
+        //    rock.transform.SetParent(part1.transform);
+        //}
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(point1.position, point2.position);
-        Gizmos.color = Color.green;
-        Vector3 uv = (point2.position - point1.position)/2;
-        Vector3 center = point1.position + uv;
-        Gizmos.DrawSphere(center, 0.5f);
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawLine(point1.position, point2.position);
+        //Gizmos.color = Color.green;
+        //Vector3 uv = (point2.position - point1.position)/2;
+        //Vector3 center = point1.position + uv;
+        //Gizmos.DrawSphere(center, 0.5f);
     }
 }
